eeb3b90bdad0a5b369995a2beaaa831b75fbf722
DESKTOP-R7E62OP\Christian L'Orange <lorange.christian@gmail.com>
2020-01-21 10:24:20 -0700

    Added analytics and changed CDN network

--- js/resources.js
+++ js/resources.js
@@ -60,7 +60,7 @@ $filterCheckboxes.on('change', function() {
 
 
   setTimeout(function(){
- $("#venues").load( "https://rawgit.com/christian-lorange/happyhour/master/hh.html" );
+ $("#venues").load( "https://raw.githack.com/christian-lorange/happyhour/master/hh.html" );
   },200);
 
 
@@ -77,7 +77,7 @@ $filterCheckboxes.on('change', function() {
 
   setTimeout(function(){
 
-   $("#modal-content").load("https://rawgit.com/christian-lorange/happyhour/master/cards.html");
+   $("#modal-content").load("https://raw.githack.com/christian-lorange/happyhour/master/cards.html");
   
    var d = new Date();
    var n = d.getDay();
